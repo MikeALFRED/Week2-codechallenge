9460493753ee05e75b949d86bc5c5593a02a585f
Paps <Mikealfredmuigai@gmail.com>
2024-03-31 18:34:28 +0300

    intial commit

---
+++ solution2.js
@@ -0,0 +1,13 @@
+// declare function to generate an arry
+function generateAnArry(start,end){
+
+// declare a new arry where we are going to store our generated numbers
+    let nad = []
+
+// we use a for lop to itterate through the array adding new numbers
+for(let i=start;i <= end; i++ ){
+    nad.push(i)
+}
+ console.log (nad)
+}
+generateAnArry(start,end)
